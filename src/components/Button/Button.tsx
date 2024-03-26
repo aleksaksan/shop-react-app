6659142c9684eb8f8e750ea785a4992befb05915
aleksand <sanyok.10rus@gmail.com>
2024-03-26 22:19:41 +0300

    added a button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
 
-export const Button = () => {
+export type ButtonProps = {
+  linkTo?: string,
+  onClick: () => void,
+  children: string,
+};
+
+export const Button = (props: ButtonProps) => {
   return (
-    <div>
-      
-    </div>
+    props.linkTo ?
+      <Link to={props.linkTo} className={styles.button}>{props.children}</Link>
+    :
+      <button onClick={props.onClick} className={styles.button}>
+        {props.children}
+      </button>
   );
 };
--- src/pages/GreetingPage/GreetingPage.tsx
+++ src/pages/GreetingPage/GreetingPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Button } from '../../components/Button/Button';
 
 export const GreetingPage = () => {
   return (
     <div>
-      
+      <Button onClick={()=>{}} >Hello!</Button>
     </div>
   );
 };
