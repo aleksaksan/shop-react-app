296caa8902f67fdb1d869cc3c18a12bee4dbe2d0
aleksand <sanyok.10rus@gmail.com>
2024-03-27 18:25:36 +0300

    added html for card

---
+++ src/components/ItemCard/ItemCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './ItemCard.module.scss';
+import { Button } from '../Button/Button';
+
+export type ItemCardProps = {
+  src?: string,
+  weight?: number,
+  price?: number,
+  title: string,
+  description?: string,
+  amount?: number,
+};
+
+export const ItemCard = (props: ItemCardProps) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.image_wrapper}>
+        <img src={props.src} alt={props.title} />
+        <div className={styles.weight}>{props.weight} г</div>
+        <div className={styles.pricw}>{props.price} ₽</div>
+      </div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+      { props.amount ?
+      <div className={styles.amount_container}>
+        <Button>-</Button>
+        <div>{props.amount}</div>
+        <Button>+</Button>
+      </div>
+      :
+      <Button>В КОРЗИНУ</Button>
+      }
+    </div>
+  );
+};
--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ItemCard } from '../../components/ItemCard/ItemCard';
 
 export const CatalogPage = () => {
   return (
     <div>
-      
+      <ItemCard title='ОРЕХ КОЛА' description='ЦЕЛЫЙ' amount={0} key={0} price={700} src='/7cecfbda-35d1-48f8-9599-4a884bc158a3.jpg' weight={20} />
     </div>
   );
 };
