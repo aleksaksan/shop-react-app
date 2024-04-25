09ad9621d5ecb4097b7d9d2efe909773a86f2d87
aleksand <sanyok.10rus@gmail.com>
2024-04-25 21:01:18 +0300

    fixed quantity buttons

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -32,7 +32,7 @@ export const CatalogPage = () => {
         error :
         <div className={styles.catalog}>
           {list.map(item => (
-            <ItemCard id={item.id} title={item.title} description={item.description} src={item.src} weight={item.weight} price={item.price} key={item.id} />
+            <ItemCard id={item.id} title={item.title} description={item.description} src={item.src} weight={item.weight} price={item.price} key={item.id} quantity={item.quantity} />
           ))}
         </div>}
     </>
