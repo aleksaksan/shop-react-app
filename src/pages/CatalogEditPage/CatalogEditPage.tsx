82a07431987707cfe09b0733acfd8a7e2a75762f
aleksand <sanyok.10rus@gmail.com>
2024-05-06 21:38:54 +0300

    added handler to DnD; fix needed

--- src/pages/CatalogEditPage/CatalogEditPage.tsx
+++ src/pages/CatalogEditPage/CatalogEditPage.tsx
@@ -1,6 +1,6 @@
-import { DndContext } from '@dnd-kit/core';
-import { SortableContext, useSortable } from '@dnd-kit/sortable';
-import React, { FC, useEffect } from 'react';
+import { DndContext, DragEndEvent } from '@dnd-kit/core';
+import { SortableContext, arrayMove, useSortable } from '@dnd-kit/sortable';
+import React, { FC, useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks/storeHooks';
 import { fetchCatalog } from '../../store/catalogSlice';
 import { ItemCard, ItemCardProps } from '../../components/ItemCard/ItemCard';
@@ -8,17 +8,34 @@ import { ItemCard, ItemCardProps } from '../../components/ItemCard/ItemCard';
 export const CatalogEditPage = () => {
   const dispatch = useAppDispatch();
   const { list } = useAppSelector(state => state.catalog)
+  const [items, setSetItems ] = useState(list);
   // const { isLoading, list, error } = useAppSelector(state => state.catalog)
 
   useEffect(() => {
     dispatch(fetchCatalog());
   }, [dispatch]);
 
+  const handleDragEnd = (event: DragEndEvent) => {
+    console.log("Drag end called");
+    const {active, over} = event;
+    console.log("ACTIVE: " + active.id);
+    console.log("OVER :" + over.id);
+
+    if(active.id !== over.id) {
+      setSetItems((items) => {
+        const activeIndex = items.indexOf(active.id);
+        const overIndex = items.indexOf(over.id);
+        console.log(arrayMove(items, activeIndex, overIndex));
+        return arrayMove(items, activeIndex, overIndex);
+      });
+    }
+  }
+
   return (
     <div>
-      <DndContext onDragEnd={()=>{}} >
-        <SortableContext items={list} >
-          {list.map(item =>
+      <DndContext onDragEnd={handleDragEnd} >
+        <SortableContext items={items} >
+          {items.map(item =>
             <SortableItem 
               key={item.id} 
               id={item.id} 
@@ -40,7 +57,7 @@ const SortableItem: FC<ItemCardProps> = (props) => {
       attributes,
       listeners,
       setNodeRef,
-  } = useSortable({id: props.id});
+    } = useSortable({id: props.id});
 
   // const style = {
   //     transform: CSS.Transform.toString(transform),
