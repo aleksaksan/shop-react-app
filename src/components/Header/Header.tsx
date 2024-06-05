c8e5ba6fcd9ecc5eac7930c57d1e5bc0e51acfc2
aleksand <sanyok.10rus@gmail.com>
2024-06-05 03:00:18 +0300

    added username to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,15 @@
+import { useTelegram } from '../../hooks/uaeTelegram';
 import { Button } from '../Button/Button';
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const { user } = useTelegram();
+
   return (
     <div className={styles.header}>
-      hello there!
+      <span className={styles.username}>
+          {user?.username}
+      </span>
       <Button linkTo='/cart'>КОРЗИНА</Button>
       <Button linkTo='/catalog'>КАТАЛОГ</Button>
     </div>
