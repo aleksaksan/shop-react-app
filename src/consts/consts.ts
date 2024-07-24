ce2e892a92808b2dfec9384a56de170f8a6cff3e
aleksand <sanyok.10rus@gmail.com>
2024-07-24 15:11:16 +0300

    Revert "changed base url for displaying photos"
    This reverts commit 7844f12bbd5141915845ec6e31612bb4de313fb7.

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,5 +1,4 @@
 // export const baseUrl = 'http://localhost:5000/';
-// export const baseUrl = 'https://express-postgresql-production-8f55.up.railway.app/';
-export const baseUrl = 'https://express-postgresql-production-8f55.up.railway.app';
+export const baseUrl = 'https://express-postgresql-production-8f55.up.railway.app/';
 export const endpointUrl = `${baseUrl}api/`;
 export const productstUrl = `${endpointUrl}products/`;
