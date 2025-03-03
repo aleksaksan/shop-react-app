f00c42fa9e457eb8ea012c7988cd7d753663b965
Aleksan <sanyok.10rus@gmail.com>
2025-03-03 21:33:40 +0300

    changed base url for backend on render

--- src/consts/consts.ts
+++ src/consts/consts.ts
@@ -1,4 +1,5 @@
-export const baseUrl = 'http://localhost:5000/';
+// export const baseUrl = 'http://localhost:5000/';
 // export const baseUrl = 'https://express-postgresql-production-8f55.up.railway.app/';
+export const baseUrl = 'https://express-postgresql-7t39.onrender.com';
 export const endpointUrl = `${baseUrl}api/`;
 export const productstUrl = `${endpointUrl}products/`;
